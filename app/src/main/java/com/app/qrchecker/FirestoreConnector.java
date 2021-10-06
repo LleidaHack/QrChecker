fbbc56f8d0b314b5295dd882e8d2ebfcdbdd3c9e
tonlls <tonlls1999@gmail.com>
2021-10-06 19:14:31 +0200

    improving connector

--- app/src/main/java/com/app/qrchecker/FirestoreConnector.java
+++ app/src/main/java/com/app/qrchecker/FirestoreConnector.java
@@ -5,31 +5,91 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirestoreConnector {
-	FirebaseFirestore db;
+	private static FirebaseFirestore db;
+	private static CollectionReference userCollection;
+	private static  CollectionReference logCollection;
+	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/mm/YYYY-HH:mm:ss");
+	private static String env="dev";
 	public FirestoreConnector(){
-		this.db = FirebaseFirestore.getInstance();
+		initFirebase();
 	}
 
-	public boolean checkUser(){
-		db.collection("users")
-				.get()
+	private void initFirebase() {
+		if(db == null)
+			db = FirebaseFirestore.getInstance();
+		if(userCollection == null)
+			userCollection = db.collection("hackeps-2021").document(env).collection("users");
+		if(logCollection == null)
+			logCollection = db.collection("hackeps-2021").document(env).collection("log");
+
+	}
+
+	public void registerUser(){
+		//Query query = citiesRef.whereEqualTo("state", "CA");
+
+	}
+	public void accessUser(String uid){
+
+		LocalDateTime now = LocalDateTime.now();
+		Map<String, Object> data = new HashMap<>();
+		data.put("time", dtf.format(now));
+
+		logCollection.document(uid)
+				.set(data)
+				.addOnSuccessListener(new OnSuccessListener<Void>() {
+					@Override
+					public void onSuccess(Void aVoid) {
+						Log.d("s", "DocumentSnapshot successfully written!");
+					}
+				})
+				.addOnFailureListener(new OnFailureListener() {
+					@Override
+					public void onFailure(@NonNull Exception e) {
+						Log.w("s", "Error writing document", e);
+					}
+				});
+	}
+	public void checkUser(){
+		Query query = userCollection.whereEqualTo("uid", "0gIqnwC6BRhREctO0Mm1kmcRpAh1");
+		query.get()
 				.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
 					@Override
 					public void onComplete(@NonNull Task<QuerySnapshot> task) {
 						if (task.isSuccessful()) {
 							for (QueryDocumentSnapshot document : task.getResult()) {
-								Log.d(TAG, document.getId() + " => " + document.getData());
+								Log.d("FIREBASE", document.getId() + " => " + document.getData());
 							}
 						} else {
-							Log.w(TAG, "Error getting documents.", task.getException());
+							Log.d("FIREBASE", "Error getting documents: ", task.getException());
 						}
 					}
 				});
+		/*userCol.get()
+				.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+					@Override
+					public void onComplete(@NonNull Task<QuerySnapshot> task) {
+						if (task.isSuccessful()) {
+							for (QueryDocumentSnapshot document : task.getResult()) {
+							}
+						} else {
+						}
+					}
+				});*/
 	}
+
 }
